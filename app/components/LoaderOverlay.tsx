b538905526adb91a76cf54a542d29fc2eaff02b8
Ankit Kumar <kumar.ankit@increff.com>
2024-10-17 17:13:19 +0530

    set default theme dark

--- app/components/LoaderOverlay.tsx
+++ app/components/LoaderOverlay.tsx
@@ -6,10 +6,10 @@ import { LuLoader2 } from "react-icons/lu";
 const LoaderOverlay = () => {
   return (
     <div
-      className="fixed inset-0 flex justify-center items-center bg-white bg-opacity-50 z-50"
+      className="fixed inset-0 flex justify-center items-center bg-white z-50"
       role="status"
     >
-      <LuLoader2 className="animate-spin h-16 w-16 text-blue-500 dark:text-blue-400" />
+      <LuLoader2 className="animate-spin h-16 w-16 text-blue-500" />
     </div>
   );
 };
--- app/context/ThemeContext.tsx
+++ app/context/ThemeContext.tsx
@@ -17,7 +17,7 @@ const ThemeContext = createContext<ThemeContextProps>({
 export const useTheme = () => useContext(ThemeContext);
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  const [theme, setTheme] = useState<Theme>("light");
+  const [theme, setTheme] = useState<Theme>("dark");
 
   useEffect(() => {
     const storedTheme = localStorage.getItem("theme") as Theme | null;
